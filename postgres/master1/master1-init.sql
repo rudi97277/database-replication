e2bc64359d680fb1d9f1962478ac636e00b1938b
rudi97277 <rudi97277@gmail.com>
2024-07-21 13:16:06 +0700

    feat: [bdr] working bdr

--- postgres/master1/master1-init.sql
+++ postgres/master1/master1-init.sql
@@ -1,15 +1,19 @@
-CREATE ROLE repluser WITH REPLICATION PASSWORD 'replpassword' LOGIN;
+-- CREATE ROLE repluser WITH REPLICATION PASSWORD 'replpassword' LOGIN;
 \c laravel
-CREATE SCHEMA master;
-CREATE SCHEMA sanctum;
+-- CREATE SCHEMA master;
+-- CREATE SCHEMA sanctum;
 
-GRANT CONNECT ON DATABASE laravel TO repluser;
-GRANT USAGE ON SCHEMA master TO repluser;
-GRANT USAGE ON SCHEMA sanctum TO repluser;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO repluser;
-GRANT SELECT ON ALL TABLES IN SCHEMA master TO repluser;
-GRANT SELECT ON ALL TABLES IN SCHEMA sanctum TO repluser;
+-- GRANT CONNECT ON DATABASE laravel TO repluser;
+-- GRANT USAGE ON SCHEMA master TO repluser;
+-- GRANT USAGE ON SCHEMA sanctum TO repluser;
+-- GRANT SELECT ON ALL TABLES IN SCHEMA public TO repluser;
+-- GRANT SELECT ON ALL TABLES IN SCHEMA master TO repluser;
+-- GRANT SELECT ON ALL TABLES IN SCHEMA sanctum TO repluser;
 
-CREATE PUBLICATION mpub1 FOR ALL TABLES;
+-- CREATE PUBLICATION mpub1 FOR ALL TABLES;
 
 -- CREATE SUBSCRIPTION msub1 CONNECTION 'host=172.21.238.5 dbname=laravel user=repluser password=replpassword' PUBLICATION mpub2;
+CREATE EXTENSION IF NOT EXISTS btree_gist;
+CREATE EXTENSION IF NOT EXISTS bdr;
+
+SELECT bdr.bdr_group_create(local_node_name := 'master1',node_external_dsn := 'host=172.21.238.4 port=5432 dbname=laravel password=rootpassword');
--- postgres/master2/master2-init.sql
+++ postgres/master2/master2-init.sql
@@ -1,16 +1,21 @@
-CREATE ROLE repluser WITH REPLICATION PASSWORD 'replpassword' LOGIN;
+-- CREATE ROLE repluser WITH REPLICATION PASSWORD 'replpassword' LOGIN;
 \c laravel
-CREATE SCHEMA master;
-CREATE SCHEMA sanctum;
+-- CREATE SCHEMA master;
+-- CREATE SCHEMA sanctum;
 
-GRANT CONNECT ON DATABASE laravel TO repluser;
-GRANT USAGE ON SCHEMA master TO repluser;
-GRANT USAGE ON SCHEMA sanctum TO repluser;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO repluser;
-GRANT SELECT ON ALL TABLES IN SCHEMA master TO repluser;
-GRANT SELECT ON ALL TABLES IN SCHEMA sanctum TO repluser;
+-- GRANT CONNECT ON DATABASE laravel TO repluser;
+-- GRANT USAGE ON SCHEMA master TO repluser;
+-- GRANT USAGE ON SCHEMA sanctum TO repluser;
+-- GRANT SELECT ON ALL TABLES IN SCHEMA public TO repluser;
+-- GRANT SELECT ON ALL TABLES IN SCHEMA master TO repluser;
+-- GRANT SELECT ON ALL TABLES IN SCHEMA sanctum TO repluser;
 
-CREATE PUBLICATION mpub2 FOR ALL TABLES;
+-- CREATE PUBLICATION mpub2 FOR ALL TABLES;
 
 
 -- CREATE SUBSCRIPTION msub2 CONNECTION 'host=172.21.238.4 dbname=laravel user=repluser password=replpassword' PUBLICATION mpub1;
+
+CREATE EXTENSION IF NOT EXISTS btree_gist;
+CREATE EXTENSION IF NOT EXISTS bdr;
+
+SELECT bdr.bdr_group_join(local_node_name := 'master2', node_external_dsn := 'host=172.21.238.5 port=5432 dbname=laravel password=rootpassword', join_using_dsn := 'host=172.21.238.4 port=5432 dbname=laravel password=rootpassword');
